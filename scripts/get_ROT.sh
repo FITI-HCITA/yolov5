10c18efe151592381a3c3d69afb3872d1f2c95c9
nick.nc.wang <nick.nc.wang@fitipower.com>
2023-07-04 17:08:11 +0800

    FIX: make CI functional
    FEAT: replace downloading data from http with DVC git

--- scripts/get_ROT.sh
+++ scripts/get_ROT.sh
@@ -6,42 +6,53 @@
 # ├── yolov5
 # └── datasets
 #     └── coco  ← downloads here
+clone_dataset_repo() {
+  git init
+  git remote add origin git@fp-Gitlab.fitipower.inc:hcita/data_center.git
+  git fetch
+  git checkout rot_ai -f
+  dvc pull
+  dvc checkout 
+}
+
 current_script_dir=$(dirname $0)
 project_dir=$current_script_dir/..
-
+data_dir=$project_dir/data
+cd $data_dir
+clone_dataset_repo
 cd $project_dir
 
-if [ -e data/datasets/SJCAM_lens-140_320x240 ]; then
-  echo "The ROT_AI dataset has already been downloaded."
-  exit 0
-fi
+# if [ -e data/datasets/SJCAM_lens-140_320x240 ]; then
+#   echo "The ROT_AI dataset has already been downloaded."
+#   exit 0
+# fi
 
-# Arguments (optional) Usage: bash data/scripts/get_coco.sh --train --val --test --segments
-if [ "$#" -gt 0 ]; then
-  for opt in "$@"; do
-    case "${opt}" in
-    --train) train=true ;;
-    --val) val=true ;;
-    --test) test=true ;;
-    --segments) segments=true ;;
-    esac
-  done
-else
-  train=true
-  val=true
-  test=false
-  segments=false
-fi
+# # Arguments (optional) Usage: bash data/scripts/get_coco.sh --train --val --test --segments
+# if [ "$#" -gt 0 ]; then
+#   for opt in "$@"; do
+#     case "${opt}" in
+#     --train) train=true ;;
+#     --val) val=true ;;
+#     --test) test=true ;;
+#     --segments) segments=true ;;
+#     esac
+#   done
+# else
+#   train=true
+#   val=true
+#   test=false
+#   segments=false
+# fi
 
-# Download/unzip labels
-#d='../data/datasets/' # unzip directory
-d='data/datasets/' # unzip directory
-d_label='data/training_cfg/labels/Data_center/'
-url=http://192.168.9.93:8000/ROT_AI/
-files="ROT_4_classes.zip IR_Extended_dataset_infer_4class.zip ROT_gray_192x192_IRLed.zip GC_IRCAM_TEST_2023-05-29.zip SA_ROT_IR_gray_192x192_20230526.zip SA_ROT_gray_IRLed_20230526.zip"
-for file in $files
-do
-  echo 'Downloading' $url$f ' ...'
-  curl -L $url$file -o data/$file -# && unzip -q data/$file -d $d && rm data/$file &
-done
-wait
+# # Download/unzip labels
+# #d='../data/datasets/' # unzip directory
+# d='data/datasets/' # unzip directory
+# d_label='data/training_cfg/labels/Data_center/'
+# url=http://192.168.9.93:8000/ROT_AI/
+# files="ROT_4_classes.zip IR_Extended_dataset_infer_4class.zip ROT_gray_192x192_IRLed.zip GC_IRCAM_TEST_2023-05-29.zip SA_ROT_IR_gray_192x192_20230526.zip SA_ROT_gray_IRLed_20230526.zip"
+# for file in $files
+# do
+#   echo 'Downloading' $url$f ' ...'
+#   curl -L $url$file -o data/$file -# && unzip -q data/$file -d $d && rm data/$file &
+# done
+# wait
