36fdc43fdd9302418d9a87038db36f0fe5e07b61
lawrence.huang(黃鍾承) <lawrence.huang@fitipower.com>
2024-06-05 07:03:39 +0000

    Correct yolov5.py IOU functions

--- postprocessor/yolov5.py
+++ postprocessor/yolov5.py
@@ -14,15 +14,16 @@ def yolov5_postprocess(output, confThr=0.7, iou_thres=0.45):
     def calculatIoU(box1, box2):
         x1 = max(box1[0], box2[0])
         y1 = max(box1[1], box2[1])
-        x2 = min(box1[0] + box1[2], box2[0] + box2[2])
-        y2 = min(box1[1] + box1[3], box2[1] + box2[3])
+        x2 = min(box1[2], box2[2])
+        y2 = min(box1[3], box2[3])
         intersection = max(0.0, x2 - x1) * max(0.0, y2 - y1)
 
-        area1 = box1[2]*box1[3] # (box1[2]-box1[0]) * (box1[3] - box1[1])
-        area2 = box1[2]*box1[3] # (box2[2]-box2[0]) * (box2[3] - box2[1])
+        area1 = (box1[2]-box1[0]) * (box1[3] - box1[1])
+        area2 = (box2[2]-box2[0]) * (box2[3] - box2[1])
         unionArea = area1 + area2 - intersection
 
         return intersection / unionArea
+
 
     def nms(bbox, iou_threshold):
         sorted_idx = np.argsort(bbox[:, 4])
